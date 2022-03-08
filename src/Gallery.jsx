3cfba4702f94a43f7f7dd75b3afd4d469bc664ec
Ben Finley <bben10hi@gmail.com>
2022-03-08 15:57:48 -0500

    added comments and changed name of the images variable to gallery

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
+// import image categories and their associated images
 import categories from './img/imageLoader';
 
+// keeps track of which images is to be displayed in the gallery
 let currentIndex = 0;
 
+// main function
 function Gallery({ categorySelection }) {
+  // category should be a numeric string representing which category the user selected
   Gallery.propTypes = {
     categorySelection: PropTypes.string.isRequired,
   };
@@ -16,18 +20,22 @@ function Gallery({ categorySelection }) {
     title = Object.keys(categories)[categorySelection];
   }
 
-  let images = Object.values(categories)[0];
+  // handles changing the group of images based on the category the user selects
+  let gallery = Object.values(categories)[0];
   if (categorySelection >= 0) {
-    images = Object.values(categories)[categorySelection];
+    gallery = Object.values(categories)[categorySelection];
   }
 
+  // highlights the first button when the page loads to match with the first image being displayed
   useEffect(() => {
     const firstButton = document.querySelector('.header button');
     firstButton.classList.add('clicked');
   }, []);
 
-  const [currentImage, setCurrentImage] = useState(Object.values(images)[currentIndex]);
+  // creates a state containing the image object being shown in the gallery
+  const [currentImage, setCurrentImage] = useState(Object.values(gallery)[currentIndex]);
 
+  // refreshes the gallery when caregory changes
   useEffect(() => {
     currentIndex = 0;
     const headerButtons = document.querySelectorAll('.header > button');
@@ -36,9 +44,10 @@ function Gallery({ categorySelection }) {
     setCurrentImage(Object.values(gallery)[currentIndex]);
   }, [categorySelection]);
 
+  // Secrolls to the next image to the left or right
   const handleScrollImage = (e) => {
     const direction = e.target.className;
-    const maxIndex = Object.values(images).length - 1;
+    const maxIndex = Object.values(gallery).length - 1;
     if (direction === 'left') {
       if (currentIndex === 0) {
         currentIndex = maxIndex;
@@ -52,19 +61,23 @@ function Gallery({ categorySelection }) {
         currentIndex += 1;
       }
     }
+    // changes the highlighting of the radial buttons to match the current image
     const headerButtons = document.querySelectorAll('.header > button');
     headerButtons.forEach((button) => button.classList.remove('clicked'));
     headerButtons[currentIndex].classList.add('clicked');
-    setCurrentImage(Object.values(images)[currentIndex]);
+    setCurrentImage(Object.values(gallery)[currentIndex]);
   };
 
+  // changes the image to the image selected on the radial buttons
   const handleChangeImage = (e) => {
     const index = parseInt(e.target.className, 10);
+    // changes the highlighting of the buttons
     const headerButtons = document.querySelectorAll('.header > button');
     headerButtons.forEach((button) => button.classList.remove('clicked'));
     currentIndex = index;
     headerButtons[currentIndex].classList.add('clicked');
-    setCurrentImage(Object.values(images)[index]);
+    // changes the image
+    setCurrentImage(Object.values(gallery)[index]);
   };
 
   return (
@@ -73,7 +86,7 @@ function Gallery({ categorySelection }) {
         <h1>{title}</h1>
         <h1>-</h1>
         {
-        Object.values(images).map((image, index) => (
+        Object.values(gallery).map((image, index) => (
           <button
             type="button"
             className={index}
