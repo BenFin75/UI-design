1ed564a9eb29b91efbbd372eb8865ac8d89aa985
Ben Finley <bben10hi@gmail.com>
2022-03-08 00:39:36 -0500

    init commit

---
+++ src/footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>
+        Created by
+        <a href="https://github.com/BenFin75"> Benjamin Finley </a>
+        c 2022
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
