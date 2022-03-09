19e945254bec31710a554e61991893cf19cbb3b2
Ben Finley <bben10hi@gmail.com>
2022-03-08 23:42:52 -0500

    sperated DropdownMenu to make it standalone

---
+++ src/DropdownMenu/DropdonwMenu.jsx
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import Icon from '@mdi/react';
+import { mdiCloseThick } from '@mdi/js';
+import componentStyle from './DropdownMenu.module.css';
+
+import categories from '../img/imageLoader';
+
+function DropdownMenu({ getCategorySelection, menuUpdater }) {
+  const [menuState, setMenuState] = useState(false);
+
+  const updateMenuState = (newState) => {
+    setMenuState(newState);
+  };
+
+  useEffect(() => {
+    // the param must be reassigned
+    // in order for useRef to be updated for callback
+    // eslint-disable-next-line no-param-reassign
+    menuUpdater.current = updateMenuState;
+  }, []);
+
+  const handleSelection = (e) => {
+    let selection;
+    if (e.target.className === 'project-tag' || e.target.className === '') {
+      selection = e.target.parentNode.className;
+    } else {
+      selection = e.target.className;
+    }
+    getCategorySelection(selection);
+    if (window.innerWidth < 500) {
+      setMenuState(false);
+    }
+  };
+
+  const handleCloseMenu = () => {
+    setMenuState(false);
+  };
+
+  return (
+    <div className={`${componentStyle.dropdown} ${menuState ? componentStyle.open : ''} `}>
+      <ul>
+        <li className={componentStyle.exit}>
+          <button
+            className={componentStyle.exitbtn}
+            type="button"
+            onClick={handleCloseMenu}
+          >
+            <Icon
+              className={componentStyle.closeIcon}
+              path={mdiCloseThick}
+              title="User Profile"
+              size={1.25}
+            />
+          </button>
+        </li>
+        {Object.keys(categories).map((category, index) => (
+          <li key={category}>
+            <button type="button" className={index} onClick={handleSelection} onKeyDown={handleSelection}>
+              {category}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+DropdownMenu.propTypes = {
+  getCategorySelection: PropTypes.func.isRequired,
+  menuUpdater: PropTypes.objectOf(PropTypes.func),
+};
+
+DropdownMenu.defaultProps = {
+  menuUpdater: null,
+};
+
+export default DropdownMenu;
